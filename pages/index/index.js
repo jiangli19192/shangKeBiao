e93718172feba34473df10637149766d1ee4ac73
unknown <unknown@example.com>
2019-11-25 23:27:01 +0800

    判断用户是否授权，获取用户登陆信息

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,21 +5,64 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    "msg": "jiangli19192",
+    userInfo: {},
+    isShowAuthButton: false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    // onLoad函数只执行一次
+    this.getUserInfo();
   },
+
+  // 根据用户是否授权，控制'获取用户登陆信息'按钮是否显示
+  // 如果用户已经授权，获取用户登陆信息
+  getUserInfo() {
+    //判断用户是否授权
+    wx.getSetting({
+      success: (res) => {
+        if (res.authSetting['scope.userInfo']) {
+          // 用户已授权
+          this.setData({
+            isShowAuthButton: false
+          });
+        } else {
+          this.setData({
+            isShowAuthButton: true
+          });
+        }
+
+        // 获取用户信息，并初始化Data.userInfo对象
+        wx.getUserInfo({
+          success: (data) => {
+            this.setData({
+              userInfo: data.userInfo
+            })
+          }
+        });
+      },
+      fail: () => {
+        console.log('获取用户信息失败!')
+      }
+    })
+  },
+
+  handleGetUserInfo(data) {
+    // 未授权回调方法
+    if (data.detail.rawData) {
+      this.onLoad();
+    }
+  },
+
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    
   },
 
   /**
