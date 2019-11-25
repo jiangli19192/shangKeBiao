50e672786317de885bd30412660318605602221b
unknown <unknown@example.com>
2019-11-25 23:10:32 +0800

    实现收藏状态从本地存储获取或保存至本地缓存

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -14,13 +14,27 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     // 获取参数index值
     let index = options.index;
     this.setData({
       detailObj: datas.list_data[index],
-      index: null
+      index
     });
+
+    // 根据本地缓存的数据判断用户是否收藏当前文章
+    let detailStorage = wx.getStorageSync('isCollected');
+    // 最开始本地缓存内没有数据情况处理
+    if (!detailStorage) {
+      // 在缓存中初始化空对象
+      wx.setStorageSync('isCollected', {});
+    }
+    // 判断用户是否收藏
+    if (detailStorage[index]) {
+      this.setData({
+        isCollected: true
+      });
+    }
   },
 
   handleCollected(event) {
@@ -35,6 +49,28 @@ Page({
     wx.showToast({
       title,
       icon: 'success'
+    });
+
+    // 获取下标值(对象的析构语法)
+    // 缓存数据格式如：{'0': true, '1': false}
+    let {
+      index
+    } = this.data;
+    // 根据KEY获取缓存本地缓存数据
+    wx.getStorage({
+      key: 'isCollected',
+      success: (datas) => {
+        let obj = datas.data;
+        obj[index] = isCollected;
+        // 缓存数据到本地
+        wx.setStorage({
+          key: 'isCollected',
+          data: obj,
+          success: () => {
+            // 缓存成功
+          }
+        })
+      }
     });
   }
 })
